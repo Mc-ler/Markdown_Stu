a61e3490642e2d562b7c0ca5820cb12efc976073
Mc-ler <change-dc@qq.com>
2026-07-21 08:42:40 +0800

    merge repository distant and local

---
+++ R Practice/calculate_GPA.py
@@ -0,0 +1,13 @@
+# Calculation My GPA of first and second year of my university.
+# Data Visualization and return GPA score approximately 
+# Edit README.md, adding .gitignore file in my project(PUll from Github?)
+
+# Import module needed:
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Read data from my score file:
+def data_extract():
+    score_data = pd.read_csv("./Markdown_Stu/R Practice/Query Grades.csv")
+    for 
