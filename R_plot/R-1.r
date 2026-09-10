6368795ad6c8e20b9adb2c443e78569e9e1478b8
Mc-ler <change-dc@qq.com>
2026-09-11 00:12:25 +0800

    Initiate my R project

--- R_plot/R-1.r
+++ R_plot/R-1.r
@@ -2,5 +2,5 @@ h <- c(1, 2, 3, 4, 5, 6)
 M <- c("A", "B", "C", "D", "E", "F")
 barplot(h,
     names.arg = M, xlab = "X", ylab = "Y",
-    col = "#0dc231", main = "Chart", border = "#e29b16", lwd = 2
+    col = "#5d8866", main = "Chart", border = "#e29b16", lwd = 2
 )
