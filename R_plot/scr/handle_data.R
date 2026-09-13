9ca87e23f603e450aad4135e8828a356641d5e9b
Mc-ler <change-dc@qq.com>
2026-09-13 11:38:37 +0800

    R Function and Clear data like pandas

--- R_plot/scr/handle_data.R
+++ R_plot/scr/handle_data.R
@@ -14,3 +14,44 @@ View(raw_data)
 summary(raw_data)
 skimr::skim(raw_data)
 
+# CLEAR DATA
+delete_reduplicate <- raw_data |> distinct(participant_id, .keep_all = TRUE)
+delete_uncomplete <- delete_reduplicate |>
+  mutate(
+    exclusion = case_when(
+      completed == 0 ~ "Unfinished",
+      attention_check == 0 ~ "fail pass the checkpoint",
+      TRUE ~ NA_character_
+    )
+  )
+wrong_data <- delete_uncomplete |> filter(!is.na(exclusion)) |> select(participant_id, exclusion)
+clear_data <- delete_reduplicate |>  filter(completed == 1, attention_check == 1)
+rename_date <- clear_data |> 
+  mutate(
+    condition = case_when(
+      condition_code == 1 ~ "High load",
+      condition_code == 2 ~ "Low load",
+      TRUE ~ NA_character_
+    ),
+    gender = case_when(
+      gender_code == 1 ~ "Famale",
+      gender_code == 2 ~ "Male",
+      TRUE ~ NA_character_
+    )
+    )
+sleep_data <- rename_date |>
+  mutate(
+    sleep_group = case_when(
+      is.na(sleep_hours) ~ NA_character_,
+      sleep_hours < 6 ~ "Less",
+      sleep_hours < 8 ~ "Moderate",
+      sleep_hours >= 8 ~ "Adequate"
+    )
+  )
+sleep_data <- sleep_data |>
+  mutate(
+    sleep_group = factor(
+      sleep_group,
+      levels = c("Less","Moderate", "Adequate")
+    )
+  )
