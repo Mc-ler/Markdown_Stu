319020b9b8092b946eca09924e9b8103f34e0e84
Mc-ler <change-dc@qq.com>
2026-09-21 01:01:19 +0800

    the first version of figure done

--- R_plot/scr/_main.R
+++ R_plot/scr/_main.R
@@ -1,9 +1,12 @@
 library("tidyverse")
 library("ggplot2")
+library("dplyr")
+library("hrbrthemes")
 
 # pipeline: import data -> depict the data set -> delete invalid data 
-# -> classify the semester -> sort the different semester -> opt(find the mostone)
-# -> plot basic histograms -> moderate fijure feature -> save as .svg or .pdf
+# -> general plot and optimize the figure surface 
+# -> classify the semester -> sort the different semester -> opt(find the most)
+# -> plot basic histograms -> moderate figure feature -> save as .svg or .pdf
 
 # IMPORT DATA
 raw_data <- read.csv("./data/Query_Grades.csv")
@@ -14,6 +17,16 @@ glimpse(raw_data)
 # clear the data and give report
 delete_reduplicate <- raw_data |> distinct(课程名, .keep_all = FALSE) # no repeat
 
-score_class <- c("A+","A-","B+","B-","C+","C-")
-valid_date <- raw_data |> filter_out(raw_data, 总成绩==score_class)
+# score_class <- c("A+","A-","B+","B-","C+","C-")
+import_data <- raw_data |> mutate(总成绩 = parse_number(总成绩))
+valid_data <- import_data |> mutate(is_valid = case_when(is.na(总成绩) ~ "invalid", TRUE ~ "valid")) |>  filter(is_valid == "valid")
+addno_data <- valid_data |> arrange(desc(总成绩)) |> mutate(No. = sprintf("%02d", 1:nrow(valid_data)))
 
+# plot the general figure 
+p <- addno_data |>
+  ggplot(aes(x = No., y = 总成绩)) +
+    geom_col( fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+    ggtitle("Plot") +
+    theme_ipsum() +
+    theme(plot.title = element_text(size = 14))
+print(p)
